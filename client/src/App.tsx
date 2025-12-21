c2837af2e7ee1c36916ffc491b2891158e9735b5
LContreras345 <luimarcontreras09@gmail.com>
2025-12-21 21:44:23 +0000

    End of section 3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { List, ListItem, ListItemText, Typography } from '@mui/material';
+import axios from 'axios';
 
 
 
@@ -9,22 +11,24 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    fetch('https://localhost:5001/api/activities')
-    .then(response => response.json())
-    .then(data => setActivities(data))
-
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+    .then(response => setActivities(response.data))
   return () => {}
   }, [])
 
   return (
-  <div>
-    <h3 className="app" style={{color: 'green'}}>Reactivities</h3>
-    <ul>
-    {activities.map((activity) => (
-      <li key={activity.id}>{activity.title}</li>
-    ))}
-    </ul>
-  </div>  
+    <>
+      <Typography variant='h3'>Reactivities</Typography>
+      <List>
+        {activities.map((activity) => (
+          <ListItem key={activity.id} alignItems="flex-start">
+            <ListItemText
+              primary={activity.title}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </>
 
   )
 }
